0999d4625af1eb1bed7941c70167fc05a064e34b
Nitzan Bueno <nitzan.bueno@gmail.com>
2016-02-11 12:37:59 +0200

    Lots of stuff
    Draw center of U now on image
    Vastly improve position location - almost done
    Lots of tests and stuff

--- CVProgram.py
+++ CVProgram.py
@@ -22,12 +22,35 @@ stream = urllib2.urlopen(urll)
 
 def drawPoint(image, point, color, thickness=2):
     x, y = int(point[0]), int(point[1])
-    cv2.rectangle(image, (x-thickness//2,y-thickness//2), (x+thickness//2,y+thickness//2), color)
+    if 0 <= x <= 600 and 0 <= y <= 800:
+        cv2.rectangle(image, (x-thickness//2,y-thickness//2), (x+thickness//2,y+thickness//2), color)
+
+
+def drawText(image, text):
+    text = str(text)
+    y0, dy = 50, 30
+    for i, line in enumerate(text.splitlines()):
+        y = y0 + i*dy
+        cv2.putText(image, line, (10, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255),1)
+
+def numpyTest(rvecs, tvecs):
+    rmat, jacobian = cv2.Rodrigues(rvecs[0])
+    tvec = tvecs[0]
+    # rtmat = np.concatenate((rmat, tvec), axis=1)
+    # rtmat = np.concatenate((rtmat, np.array([[0,0,0,1]])), axis=0)
+    # cameramat = np.linalg.inv(rtmat)
+    # camR = cameramat[0:3, 0:3]
+    # camT = cameramat[0:3, 3]
+    camR = rmat.T
+    camT = -np.dot(rmat, tvec)
+    return camR, camT
+
 
 # This is the point whose HSL values are displayed on screen as well as the point itself as a rectangle
 track = (100,100)
-
-points = np.array([[[-267.5,305,0], [267.5, 305, 0], [267.5, 0, 0], [-267.5, 0, 0]]]).astype("float32")
+np.set_printoptions(precision=1)
+np.set_printoptions(suppress=True)
+points = np.array([[[268.5,305,0], [-268.5, 305, 0], [-268.5, 0, 0], [268.5, 0, 0]]]).astype("float32")
 ding = True
 bites=''
 while True:
@@ -53,6 +76,7 @@ while True:
     lower = np.array([30,40,60])
     upper = np.array([80,160,255])
     filtered = cv2.inRange(hslimage, lower, upper)
+    #cv2.imshow("Filtered", filtered)
 
     # Find corners of U: Find contour and then do a convex hull and approxPolyDP, then we'll have a 4-gon of corners.
     contours, hierarchy = cv2.findContours(filtered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -86,26 +110,28 @@ while True:
                     break
             cv2.drawContours(image, [poly], -1, (255, 255, 255), thickness=4)
             drawPoint(image, poly[trindex, 0], (255, 0, 0), thickness=10)
+            drawPoint(image, poly[(trindex+1)%4, 0], (0,255,255), thickness=10)
             # Now comes the problematic part - the camera calibration (or position location).
             corners = np.array([[poly[(i+trindex)%len(poly),0] for i in range(len(poly))]]).astype("float32")
             retval, matrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(points, corners, (600,800), None, flags=cv2.CALIB_USE_INTRINSIC_GUESS)
             tvec = tvecs[0]
+            rvec = rvecs[0]
+            camR, camT = numpyTest(rvecs, tvecs)
+            dist = np.linalg.norm(camT)
+            camRVec, jacobian = cv2.Rodrigues(camR)
+            drawText(image, camRVec*10)
+            origin = np.array([[0],[2],[0]])
+            rmat, jacobian = cv2.Rodrigues(rvec)
+            center, jacobian = cv2.projectPoints(np.float32([[0,0,0]]), rvec, tvec, matrix, distCoeffs)
+            drawPoint(image, center[0,0], (0, 255, 255), 10)
             p = (sum([i[0]**2 for i in tvec]))**(0.5)
             p = int(p)
-            rvec = rvecs[0]
             drawPoint(image, tvec, (255, 0, 0))
-            cv2.putText(image,str(p),(10,50), font, 1,(255,255,255),2)
-            cv2.line(image, (400, 0), (400, 600), (0, 255, 0))
-            center, jacobian = cv2.projectPoints(np.float32([[0,152,0]]), rvec, tvec, matrix, distCoeffs)
-            acc = 100
-            if (abs(center[0,0,0] - 400) < cv2.arcLength(cont, True) / acc):
-                drawPoint(image, center[0,0], (0, 255, 0), 10)
-            else:
-                drawPoint(image, center[0,0], (0, 255, 255), 3)
+            drawPoint(image, np.add(np.dot(rmat, origin), tvec), (255, 255, 255))
 
     # Show results
     #cv2.putText(image,str(hslimage[track[1], track[0]]),(10,50), font, 1,(255,255,255),2)
-    drawPoint(image, track, (0,255,0))
+    #drawPoint(image, track, (0,255,0))
     cv2.imshow('Image',image)
     k = cv2.waitKey(1) & 0xFF
     if k == ord("q"):
